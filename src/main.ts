e80eebc6e753c45531dc5e899ebb1aaf2c7119c6
Shihab <cshihab04@gmail.com>
2021-08-16 11:02:41 +0600

    testing 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { Controller, Module, Get } from "@nestjs/common";
+import { Controller, Module, Get } from '@nestjs/common';
 
-//decorator
 @Controller()
-class AppController{
+class AppController {
     @Get()
     getRootRoute() {
         return 'hi there!';
